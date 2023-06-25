acf35e8effdec26073674993f7d1b29825053f12
kwonkangbin <kbk282655@gmail.com>
2023-06-25 23:07:43 +0900

    이것저것

--- quiz/models.py
+++ quiz/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Quiz(models.Model):
-    title = models.CharField(max_lenth = 200)
+    title = models.CharField(max_length = 200)
     body = models.TextField()
-    answer = models.IntergerField()
+    answer = models.IntegerField()
